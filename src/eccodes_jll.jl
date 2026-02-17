72ce0a62204abd485c3d0cf679b133999c5f323c
jlbuild <juliabuildbot@gmail.com>
2026-02-17 14:13:34 +0000

    eccodes_jll build 2.45.0+0

--- src/eccodes_jll.jl
+++ src/eccodes_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("eccodes")
-JLLWrappers.@generate_main_file("eccodes", UUID("b04048ba-5ccd-5610-b3f6-85129a548705"))
+JLLWrappers.@generate_main_file("eccodes", Base.UUID("b04048ba-5ccd-5610-b3f6-85129a548705"))
 end  # module eccodes_jll
